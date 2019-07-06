37cb89fc3160f8a078b2a65d6b58384b34ef3dc1
koen van de velde <koenvandevelde@gmail.com>
2019-07-06 18:33:20 +0200

    data-exploration

---
+++ data-exploration.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df_all = pd.read_csv("data/creditcard.csv")
+df_non_fraudulent = df_all[df_all.Class == 0]
+
+# print('describe dataset')
+# print(df.head(5))
+# print(df.describe())
+# print(df.tail(5))
+
+
+def plotColumnValues(dfType, df, column_title):
+    column = column_title
+    title = column + '_' + dfType + '_exploratory_analysis'
+    fig = plt.figure(title)
+    plt.plot(pd.to_numeric(df[column]))
+    plt.title(title)
+    plt.ylabel(column)
+    plt.draw()
+    plt.savefig('data/exploration/' + title)
+    plt.close()
+
+def plotColumnHistogram(dfType, df, column_title):
+    column = column_title
+    title = column + '_' + dfType + '_exploratory_histogram'
+    fig = plt.figure(title)
+    plt.hist(pd.to_numeric(df[column]))
+    plt.title(title)
+    plt.ylabel(column)
+    plt.draw()
+    plt.savefig('data/exploration/' + title)
+    plt.close()
+
+
+def analyse(dfType, df):
+    for x in range(1, 29):
+        print('describe')
+        dataFrame = df['V' + str(x)]
+        print(dataFrame.describe())
+        column = 'V' + str(x)
+        plotColumnValues(dfType, df, column)
+        plotColumnHistogram(dfType, df, column)
+    plotColumnValues(dfType, df, 'Time')
+    plotColumnHistogram(dfType, df, 'Time')
+    plotColumnValues(dfType, df, 'Amount')
+    plotColumnHistogram(dfType, df, 'Amount')
+    plotColumnValues(dfType, df, 'Class')
+    plotColumnHistogram(dfType, df, 'Class')
+
+analyse('all', df_all)
+analyse('non-fraudulent', df_non_fraudulent)
+
